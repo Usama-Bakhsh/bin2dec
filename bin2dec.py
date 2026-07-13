540c1b2d7816e4d4e8ebe50994ff95340367a193
Usama-Bakhsh <UsamaBakhsh@pm.me>
2026-07-13 21:27:16 +0200

    Decimal til hexadecimal funktion implementeret

--- bin2dec.py
+++ bin2dec.py
@@ -6,10 +6,25 @@ def bin2dec(input_str):
         output = (output * 2) + int(digit)
     return output
 
+hex_table = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 
+                 7:'7', 8:'8', 9:'9', 10:'A', 11:'B', 12:'C', 13:'D', 
+                 14:'E', 15:'F',}
+
+def dec2hex(input_dec):
+    output = ''
+    while(input_dec > 0):
+        remainder = input_dec % 16
+        output = hex_table[remainder] + output
+        input_dec = input_dec // 16
+
+    return output
+
 if __name__ == "__main__":
     user_input = input("Indtast et binært tal: ")
     try:
-        result = bin2dec(user_input)
-        print(f"Decimal værdi: {result}")
+        dec_output = bin2dec(user_input)
+        print(f"Decimal værdi: {dec_output}")
+        hex_output = dec2hex(dec_output)
+        print(f"Hexadecimal værdi: {hex_output}")
     except ValueError as e:
         print(f"Fejl: {e}")
